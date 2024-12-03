a546743cae0b673a830dff73d1478295a70ff8c6
mszusta <capgemini.szustak@extaccount.com>
2024-12-03 15:08:39 +0100

    day 3 of training

---
+++ day_3/odd_or_even.py
@@ -0,0 +1,10 @@
+# If/else training and modulo
+# Check if number in the input area is odd or even.
+
+print("Code to check if provided number is odd or even")
+number = int(input("Provide a number: "))
+
+if number % 2 == 0:
+    print("Number is Even")
+else:
+    print("Number is Odd")
