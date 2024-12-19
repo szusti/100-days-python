5cfbaa2f27c3bd73c9140c6da3b5706ac0ef86b7
mszusta <capgemini.szustak@extaccount.com>
2024-12-19 14:03:37 +0100

    day-10 and fix for day 9

--- day-9/secret-auction/bidding_auction.py
+++ day-9/secret-auction/bidding_auction.py
@@ -7,14 +7,17 @@ def add_bidder():
     bidders_list[name] = price
 
 def select_winner():
-    highest_bid = 0
+    # fload -ing instead 0 is a good practice due to numbers below 0
+    highest_bid = float('-inf')
     winning_person = ""
     for person in bidders_list:
         if bidders_list[person] > highest_bid:
             winning_person = person
             highest_bid = bidders_list[person]
-    # above can be shortening with max
-    # winning_person = max(bidders_list.get)
+    # above can be shortening with max function
+    # winning_person = max(bidders_list, key=bidders_list.get)
+    # if you would want highest value instead (just remove "key=")
+    # winning_person = max(bidders_list, bidders_list.get)
     return winning_person
 
 
@@ -31,7 +34,6 @@ while another_person:
         another_person=False
     system("cls")
 
-print(bidders_list)
 bid_winner = select_winner()
 
 print(f"The winner is\n{bid_winner} : {bidders_list[bid_winner]}PLN")
